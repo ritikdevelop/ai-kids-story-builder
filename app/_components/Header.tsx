9c7f544f682041019cf7f071d2977627b9c3452e
ritikdevelop <hrithikrana216@gmail.com>
2024-11-03 01:40:51 +0530

    first commit

---
+++ app/_components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+
+
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
