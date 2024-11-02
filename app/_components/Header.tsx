8b8f79af249b5c14eedd49ffb000e71b38e294ca
ritikdevelop <hrithikrana216@gmail.com>
2024-11-03 02:02:45 +0530

    "Updated Header component with new design and navigation, and added it to the page layout."

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -8,10 +8,54 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-
+import Image from "next/image";
+import path from "path";
+import Link from "next/link";
+import { Button } from "@nextui-org/button";
 
 function Header() {
-  return <div>Header</div>;
+  const MenuList = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Create Story",
+      path: "/create-story",
+    },
+    {
+      name: "Explore Stories",
+      path: "/explore",
+    },
+    {
+      name: "Contact Us",
+      path: "/contact-us",
+    },
+  ];
+
+  return (
+    <Navbar maxWidth="full">
+      <NavbarContent>
+        <NavbarBrand>
+          <Image src={"/logo.svg"} alt="Logo" width={40} height={40} />
+          <h2 className="font-bold text-2xl text-primary ml-3">Kids Story</h2>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify="center">
+        {MenuList.map((item, index) => (
+          <NavbarItem className="text-xl text-primary font-medium hover:underline mx-2">
+            <Link href={item.path}>{item.name}</Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <Button color="primary">
+            Get Started
+        </Button>
+      </NavbarContent>
+    </Navbar>
+  );
 }
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello, World!!</h2>
-      <Button>
-        search
-      </Button>
+    <div className="bg-[#cad3ff]">
+      {/* Header */}
+      <Header />
     </div>
   );
 }
