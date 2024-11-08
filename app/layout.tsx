96fd69584dd863e6fd3de07ed5b9af88a84142e6
ritikdevelop <hrithikrana216@gmail.com>
2024-11-09 00:09:37 +0530

    "Updated Hero component with minor formatting changes and commented out font imports in layout.tsx"

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,22 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Provider from "./provider";
-import {Montserrat} from 'next/font/google';
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
-  subsets: ['latin']
+  subsets: ["latin"],
 });
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -32,9 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={montserrat.className}
-      >
+      <body className={montserrat.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
